392aa51da45b8cc4de7684bcc299b7e1fd76a349
Tom Scholten <git@scholten.nu>
2020-11-09 20:19:36 +0100

    Action setup - add cleanup

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -x
 clonedir="$(pwd)/clones"
 prdir="${clonedir}/pw-core/homeassistant/components/plugwise/"
+rm index.html diff*html bm_diff*html
 cd ${prdir}
 git remote add upstream https://github.com/home-assistant/core.git || exit 0
