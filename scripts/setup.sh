5621aaf4f7a96994e005cae27da503f08e4b77dd
Tom Scholten <git@scholten.nu>
2022-03-01 16:24:06 +0100

    Shellcheck

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,5 +2,5 @@
 clonedir="$(pwd)/clones"
 prdir="${clonedir}/pw-core/homeassistant/components/plugwise/"
 rm index.html diff*html bm_diff*html
-cd ${prdir}
+cd "${prdir}" || exit
 git remote add upstream https://github.com/home-assistant/core.git || exit 0
