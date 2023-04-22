602809a69ae237e096aeeb4c1261747744ab371a
Tom Scholten <git@scholten.nu>
2023-04-22 20:26:10 +0200

    Clean up for publishing

--- scripts/diffy.sh
+++ scripts/diffy.sh
@@ -124,25 +124,3 @@ echo "
         </body>
 </html>" >> "${pdir}/index.html"
 
-echo "<html>
-        <head>
-                <title>Plugwise beta's (USB/Networked) vs Core progress</title>
-        </head>
-        <body>
-                        <li><a href='beta/index.html'>Plugwise Smile beta</a>
-                        <li><a href='usb-beta/index.html'>Plugwise USB beta</a>
-
-	</body>
-</html>" >> "${rdir}/index.html"
-
-cd "${pdir}" || exit
-pwd
-ls -alrt
-
-
-#git remote set-url origin https://x-access-token:${PROGRESS_DEPLOYKEY}@github.com/$GITHUB_REPOSITORY
-git checkout "${GITHUB_HEAD_REF}"
-git add -A 
-git commit -m "Update: ${GITHUB_REF##*/} - Diff report completed"
-git push origin "${GITHUB_REF##*/}"
-
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,8 +2,6 @@
 if [ "${1}" == "" ]; then
 	echo "Syntax: ${0} {beta|usb-beta}"
 fi
-pwd
-find ./ -type d
 clonedir="$(pwd)/clones/${1}"
 prdir="${clonedir}/pw-core/homeassistant/components/plugwise/"
 rm index.html "${1}/index.html" "${1}/diff*html" "${1}/bm_diff*html" || :
