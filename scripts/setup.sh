bcdc21d7a02e6a1f97b81f22aaec1e85ff8063fa
Tom Scholten <git@scholten.nu>
2023-04-22 15:37:20 +0200

    Handle clones

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -4,6 +4,6 @@ if [ "${1}" == "" ]; then
 fi
 clonedir="$(pwd)/${1}/clones"
 prdir="${clonedir}/pw-core/homeassistant/components/plugwise/"
-rm index.html "${1}/index.html" "${1}/diff*html" "${1}/bm_diff*html"
+rm index.html "${1}/index.html" "${1}/diff*html" "${1}/bm_diff*html" || exit 0
 cd "${prdir}" || exit
 git remote add upstream https://github.com/home-assistant/core.git || exit 0
